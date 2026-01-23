c930be92dfd12bbbbf4b3323cabe639c4d8cbaff
Ray <2167250556david@gmail.com>
2026-01-23 23:54:39 +0800

    nop

---
+++ a+b计算.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+    return 0;
+}
---
+++ 赋值与计算.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i=100;
+    int j;
+    j=i+10;
+    printf("%d\n", j);
+    return 0;
+}
+
